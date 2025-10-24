5a289a6958a85357fc29e981eaf15599f02983f6
RafaelDeps <108770110+RafaelDeps@users.noreply.github.com>
2025-10-24 10:15:31 -0300

    Add files via upload

---
+++ teste_java/script.js
@@ -0,0 +1,15 @@
+function verificacao(){
+    let nomeInput = document.getElementById('name');
+    let idadeInput = document.getElementById('idade');
+    let mensagemResultado = document.getElementById('resultado');
+
+    let nome = nomeInput.value.trim();
+    let idade = parseInt(idadeInput.value);
+
+    let vereador = 18;
+    let prefeitoDeputadoJuizPaz = 21;
+    let  governador = 25
+    let senadorPresidente = 35;
+
+
+}
